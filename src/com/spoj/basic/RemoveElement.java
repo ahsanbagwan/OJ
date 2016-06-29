7c9f2bda9eee4402f3c2fe81717b935327c069b5
Ahsan Bagwan <ahbagwan@cisco.com>
2016-06-29 17:45:56 +0530

    remove array

--- src/com/spoj/basic/RemoveElement.java
+++ src/com/spoj/basic/RemoveElement.java
@@ -1,15 +1,29 @@
 package com.spoj.basic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by ahbagwan on 6/29/2016.
  */
 public class RemoveElement {
-    public int removeElement(int[] elements, int val) {
+    // TODO wrong please correct
+    public static int removeElement(Integer[] elements, int val) {
         int result = 0;
-        for (int i=0; i<elements.length; i++){
+        for (int i=0; i < elements.length-1; i++){
             if ( elements[i] == val ) {
-                
+                elements[i] = elements[i+1];
             }
         }
+        List<Integer> list = Arrays.asList(elements);
+
+        return Collections.frequency(list, val);
+    }
+
+    public static void main(String[] args) {
+        int frequency = RemoveElement.removeElement(new Integer[]{3,2,2,3}, 3);
+        System.out.println("frequency " + frequency );
     }
 }
