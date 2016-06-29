884a010a63b6c9c44e9c468be5e69283dabbb451
Ahsan Bagwan <ahbagwan@cisco.com>
2016-06-29 17:19:32 +0530

    remove array

---
+++ src/com/spoj/basic/RemoveElement.java
@@ -0,0 +1,15 @@
+package com.spoj.basic;
+
+/**
+ * Created by ahbagwan on 6/29/2016.
+ */
+public class RemoveElement {
+    public int removeElement(int[] elements, int val) {
+        int result = 0;
+        for (int i=0; i<elements.length; i++){
+            if ( elements[i] == val ) {
+                
+            }
+        }
+    }
+}
