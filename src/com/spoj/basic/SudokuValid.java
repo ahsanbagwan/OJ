a031dbeed04100d0d27a3370dc23aa62c578dcf6
Ahsan Bagwan <ahbagwan@cisco.com>
2016-06-29 20:35:27 +0530

    sudoku solver

---
+++ src/com/spoj/basic/SudokuValid.java
@@ -0,0 +1,22 @@
+package com.spoj.basic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ahbagwan on 6/29/2016.
+ */
+public class SudokuValid {
+    private static final board[][] = {}
+
+    private Map<Integer, Integer> columnMap = new HashMap<>();
+    private Map<Integer, Integer> rowMap = new HashMap<>();
+    private Map<Integer, Integer> gridMap = new HashMap<>();
+
+    public boolean isValidSudoku(char[][] board) {
+        // each horizontal row contains a digit once
+        // each vertical column contains a digit once
+        // each grid contains a digit once
+
+    }
+}
